9f0a9764f5d7841fbc842d757da30aa1578e95d8
Adams Temidire A <gr8temi@gmail.com>
2019-08-22 00:26:09 +0100

    create login page

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -10,6 +10,18 @@ from allauth.account import views as all_views
 from allauth.socialaccount import views as so_views
 urlpatterns = [
 	url(r'^accounts/', include('allauth.urls')),
-	url(r'^login/$', all_views.LoginView.as_view(), {'template_name':'Accounts/index.html'}, name='account_login'),
-	url(r'^SignUp/$', all_views.SignupView.as_view(), {'template_name':'Accounts/signup.html'}, name='account_signup'),
-	]
+	url(r'^login/$', all_views.LoginView.as_view(), {'template_name':'account/login.html'}, name='account_login'),
+	url(r'^SignUp/$', all_views.SignupView.as_view(), {'template_name':'account/signup.html'}, name='account_signup'),
+	url(r'^Mail_activate/$', all_views.email_verification_sent, name='account_email_verification_sent'),
+	url(r"^confirm-email/(?P<key>[-:\w]+)/$", all_views.confirm_email,
+        name="account_confirm_email"),
+	url(r'^signup/$', so_views.SignupView.as_view(),{'template_name':'socialaccount/signup.html'}, name='socialaccount_signup'),
+	url(r'^logout/$', auth_views.logout, name='logout'),
+	# url(r'^Update-Info/(?P<pk>\d+)/$', views.Info.as_view(), name='info'),
+	# url(r'^register/$', views.UserFormView.as_view(), name='register'),
+	# url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',views.activate, name='activate'),
+	# url(r'^password_reset/$',password_reset,{'template_name':'account/password_reset_form.html'}, name='password_reset'),
+	# url(r'^password_reset/done/$', auth_views.password_reset_done, {'template_name':'account/password_reset_done.html'}, name='password_reset_done'),
+	# url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm,{'template_name':'account/password_reset_confirm.html'}, name='password_reset_confirm'),
+	# url(r'^reset/done/$', auth_views.password_reset_complete,{'template_name':'account/password_reset_complete.html'}, name='password_reset_complete'),
+]
